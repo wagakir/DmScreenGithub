b6576b78189eabcc89d645210164354561de2549
wagakir <125695677+wagakir@users.noreply.github.com>
2024-04-09 19:11:04 +0500

    css update

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
-let btn= document.getElementById('sortInitiative')
+let sortBtn= document.getElementById('sortInitiative')
 let playersList= document.getElementById('playersList')
 
 
-function btnclick(){playersList.append(`<p>`)}
+function btnclick(){
+    let playerDiv = document.createElement('div')
+    playerDiv.innerHTML = `
+    <p>index №1</p>
+    <p>13</p>
+    <p>20hp</p>
+    <p>имя</p>
+    <button>смэрть</button>`
+    playerDiv.setAttribute()
+    playersList.append(playerDiv)}
 
 
 
-btn.addEventListener('click', btnclick)
+sortBtn.addEventListener('click', btnclick)
 
