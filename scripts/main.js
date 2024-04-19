00120c49d4ea5842ef05f22859c99535e83d6e0c
wagakir <125695677+wagakir@users.noreply.github.com>
2024-04-19 18:02:04 +0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,24 @@
 let sortBtn= document.getElementById('sortInitiative')
 let playersDiv= document.getElementById('playersDiv')
 let playerAddBtn= document.getElementById('playerAddBtn')
-let index = 1
+let index = 0
 let body = document.body
 let npcsDiv= document.getElementById('npcDiv')
 let npcAddBtn= document.getElementById('npcAddBtn')
-let playersLists = []
+// let playersLists = []
 let initiativeList = document.getElementById('initiativeList')
-function sort(){
 
 
-}
-
-
+sortBtn.onclick = function () {alert('sosi bibu')}
 
 //to delete later
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 //end del
-function appendPlayer() {
+function appendPlayer(whoAppend) {
 
+    let playersDiv= document.getElementById(whoAppend)
     // ${initiativeInput}
     let playerInitiativeInput =  getRandomInt(20)//document.querySelector('.playerInitiativeInput').value
     let playerHPInput = document.querySelector('.playerHPInput').value
@@ -50,46 +48,37 @@ function appendPlayer() {
 
 
     playersDiv.append(playerDiv)
+    // let playerList  = [index, playerInitiativeInput, playerHPInput, playerMaxHPInput, playerNameInput, initiativeDiv.id ]
+    // playersLists.push( [ playerList])
+    //
+    // // console.log(playerList)
+    // let bebe = (playersLists[0])
+
+
+
 
     let players = document.querySelectorAll('.player')
     players.forEach(element => {
-
         element.childNodes.forEach(cldElement =>{
 
-            cldElement.onchange = function () {
+                function sort() {
                 document.querySelector('#initiativeList').childNodes.forEach(initiativeCldElement =>{
-
                     let elementId = '#' + String(element.id)
                     let initiativeCldElementId = '#' + String(initiativeCldElement.id)
-
                     if(initiativeCldElementId.slice(18) === elementId.slice(7)){
-                        // let list = document.querySelector(`${initiativeCldElementId}   `).childNodes
-                        console.log(element.querySelector('.playerName'))
-
                         initiativeCldElement.innerHTML = `
                           <div>Initiative ${element.querySelector('.playerInitiative').value}</div>
                            <div class="playerNameInitiative" style="width: fit-content">${element.querySelector('.playerName').value}</div>`
-
+                        elementId.slice(7)[0][1] = element.querySelector('.playerInitiative').value
+                        console.log(element.querySelector('.playerInitiative').value)
+                        initiativeCldElement.style.order = element.querySelector('.playerInitiative').value
                     }
-
-
-                    // if (element.id[-1] == initiativeCldElement.id[-1]){
-                    //     initiativeCldElement.id.innerHTML = `
-                    //       <div>Initiative ${playerDiv.firstElementChild.querySelector('.playerInitiative').value}</div>
-                    //        <div class="playerNameInitiative" style="width: fit-content">${playerDiv.firstElementChild.querySelector('.playerName').value}</div>`
-                    // }
                 })
-                //alert(playerDiv.firstElementChild.querySelector('.playerInitiative').value)
 
-                 //document.querySelector(`#initiativeElement${index}`).innerHTML = `
-                  //  <div>Initiative ${playerDiv.firstElementChild.querySelector('.playerInitiative').value}</div>
-                 //   <div class="playerNameInitiative" style="width: fit-content">${playerDiv.firstElementChild.querySelector('.playerName').value}</div>`
                 }
+            cldElement.onchange = sort
+            sort()
         })
-        // document.getElementById(`initiativeElement${index}`)
-        // element.onchange = function () {
-        //     initiativeDiv.innerHTML = `<div>Initiative ${playerInitiativeInput}</div> <div class="playerNameInitiative" style="width: fit-content">${playerNameInput}</div>`
-        // }
     });
 
     //add delete btn function
@@ -97,17 +86,13 @@ function appendPlayer() {
     DeleteBtn.forEach(element => {
 
         element.onclick = function () {
-            console.log()
             let toDelete = String( '#initiativeElement' + this.parentNode.id.slice(6))
             document.querySelector(toDelete).remove()
             this.parentNode.remove()
         }
     });
 
-    let playerList  = [index, playerInitiativeInput, playerHPInput, playerMaxHPInput, playerNameInput, initiativeDiv.id ]
-    playersLists.push( [ playerList])
-    console.log(playerList)
-    console.log(playersLists)
+
     index +=1
 }
 
@@ -123,7 +108,7 @@ function appendNpc() {
     }
 
         npcDiv.innerHTML = `
-        <div style="flex-grow: 1; display: flex; flex-direction: column">
+        <div style="flex-grow: 1; display: flex; flex-direction: column; height: fit-content" >
         <div>Initiative <input class="npcInitiative" value="${npcInitiativeInput}"> <input class="npcName"   value="${npcNameInput}"></div> 
         <div><label>HP<input class="npcHP" value="${npcHPInput}">/<input class="npcHP" value="${npcMaxHPInput}"></label> </div></div> 
     
@@ -148,8 +133,8 @@ themeBtn.forEach(element => {
     }
 });
 
+'npcDiv'
 
-sortBtn.addEventListener('click',sort)
-
-playerAddBtn.addEventListener('click', appendPlayer)
-npcAddBtn.addEventListener('click',appendNpc)
+playerAddBtn.onclick = function (){appendPlayer('playersDiv')}
+// playerAddBtn.addEventListener('click', appendPlayer )
+npcAddBtn.onclick = function (){appendPlayer('npcDiv')}
