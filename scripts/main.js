9c70dcf0e04722feb28181c263d9804ee2139ded
wagakir <125695677+wagakir@users.noreply.github.com>
2024-05-11 21:48:45 +0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,14 @@
-let sortBtn= document.getElementById('sortInitiative')
+//let sortBtn= document.getElementById('sortInitiative')
 let playersDiv= document.getElementById('playersDiv')
 let playerAddBtn= document.getElementById('playerAddBtn')
 let index = 0
 let body = document.body
-let npcsDiv= document.getElementById('npcDiv')
+
 let npcAddBtn= document.getElementById('npcAddBtn')
 // let playersLists = []
 let initiativeList = document.getElementById('initiativeList')
 
 
-sortBtn.onclick = function () {alert('sosi bibu')}
 
 //to delete later
 function getRandomInt(max) {
@@ -32,7 +31,7 @@ function appendPlayer(whoAppend) {
     playerDiv.innerHTML = `
     <div style="flex-grow: 1; display: flex; flex-direction: column">
         <div>Initiative <input class="playerInitiative" value="${playerInitiativeInput}"> <input class="playerName"   value="${playerNameInput}"></div> 
-        <div><label>HP<input class="playerHP" value="${playerHPInput }">/<input class="playerHP" value="${playerMaxHPInput }"></label> </div></div> 
+        <div><label>HP<input class="playerHP" value="${playerHPInput }">/<input class="playerHP" value="${playerMaxHPInput }"></label> <input class="playerName" ></div></div> 
     <button class="delete">смэрть</button>`;
 
     playerDiv.className = 'player'
@@ -66,12 +65,18 @@ function appendPlayer(whoAppend) {
                     let elementId = '#' + String(element.id)
                     let initiativeCldElementId = '#' + String(initiativeCldElement.id)
                     if(initiativeCldElementId.slice(18) === elementId.slice(7)){
+                        if (isNaN(element.querySelector('.playerInitiative').value)){
+                            alert('check initiative, is should be a number')
+                            return
+                        }
                         initiativeCldElement.innerHTML = `
                           <div>Initiative ${element.querySelector('.playerInitiative').value}</div>
                            <div class="playerNameInitiative" style="width: fit-content">${element.querySelector('.playerName').value}</div>`
                         elementId.slice(7)[0][1] = element.querySelector('.playerInitiative').value
                         console.log(element.querySelector('.playerInitiative').value)
-                        initiativeCldElement.style.order = element.querySelector('.playerInitiative').value
+                        let initiative = element.querySelector('.playerInitiative').value
+                        initiative = initiative * -1
+                        initiativeCldElement.style.order = initiative
                     }
                 })
 
@@ -91,40 +96,20 @@ function appendPlayer(whoAppend) {
             this.parentNode.remove()
         }
     });
+    initiativeList.childNodes.forEach( element =>{
+
+        element.onclick = function (){
+            this.classList.toggle('border = solid rgba(255,255,255, 0.7) 10px');
+            this.classList.toggle('completed');
+        }
+    });
 
 
-    index +=1
+
+    index += 1
 }
 
-function appendNpc() {
-    let npcInitiativeInput = document.querySelector('.npcInitiativeInput').value
-    let npcHPInput = document.querySelector('.npcHPInput').value
-    let npcMaxHPInput = document.querySelector('.npcMaxHPInput').value
-    let npcNameInput = document.querySelector('.npcNameInput').value
-    let npcDiv = document.createElement('div');
-    if (isNaN(npcInitiativeInput)){
-        alert("введите инициативу")
-        return;
-    }
 
-        npcDiv.innerHTML = `
-        <div style="flex-grow: 1; display: flex; flex-direction: column; height: fit-content" >
-        <div>Initiative <input class="npcInitiative" value="${npcInitiativeInput}"> <input class="npcName"   value="${npcNameInput}"></div> 
-        <div><label>HP<input class="npcHP" value="${npcHPInput}">/<input class="npcHP" value="${npcMaxHPInput}"></label> </div></div> 
-    
-        <button class="delete">смэрть</button>`;
-        npcDiv.className = 'npc'
-
-        npcsDiv.append(npcDiv)
-        let DeleteBtn = document.querySelectorAll('.delete')
-        DeleteBtn.forEach(element => {
-            element.onclick = function () {
-                this.parentNode.remove()
-            }
-        });
-        index += 1
-
-}
 
 let themeBtn = document.querySelectorAll('.theme')
 themeBtn.forEach(element => {
@@ -133,7 +118,7 @@ themeBtn.forEach(element => {
     }
 });
 
-'npcDiv'
+
 
 playerAddBtn.onclick = function (){appendPlayer('playersDiv')}
 // playerAddBtn.addEventListener('click', appendPlayer )
